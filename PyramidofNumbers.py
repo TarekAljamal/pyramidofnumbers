936664cfb76605254842975ec4a18f38e7393627
Tarek Aljamal <tarekaljamal@outlook.com>
2024-10-30 15:11:52 +0200

    pyramid of numbers

---
+++ PyramidofNumbers.py
@@ -0,0 +1,12 @@
+# A python code that requests the user to enter a number and displays a pyramid having number of raws equal to this number
+
+x = input(" Enter a number x to display a pyramid: ")
+
+for x in range(1, int(x)+1, 1):
+     for i in range(1, int(x+1), 1):
+          print(i, end="")
+     print()    
+          
+          
+    
+          
